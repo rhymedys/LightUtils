6844190d29a7a1f9addf84d3dba83fae897b6c0d
rhymedys <rhymedys@gmail.com>
2017-12-15 21:04:59 +0800

    update

--- test/unit/specs/PathUtils.spec.js
+++ test/unit/specs/PathUtils.spec.js
@@ -13,16 +13,17 @@ describe('utils',()=>{
   })
 
   it('can tell if a function _getUrlPathByGrade pass',()=>{
-    console.log(utils._getUrlPathByGrade(window.location,0))
-    console.log(utils._getUrlPathByGrade(window.location.href,1))
-    console.log(utils._getUrlPathByGrade(window.location.href,2))
-    console.log(utils._getUrlPathByGrade(window.location.href,3))
+    let test1='https://github.com/karma-runner/karma'
+    expect(utils._getUrlPathByGrade(test1,0)).toBe(undefined)
+    expect(utils._getUrlPathByGrade(test1,1)).toBe('https://github.com')
+    expect(utils._getUrlPathByGrade(test1,2)).toBe('https://github.com/karma-runner')
+    expect(utils._getUrlPathByGrade(test1,3)).toBe('https://github.com/karma-runner/karma')
+    
   })
 
   it('can tell if a function _replaceUrlProtocal2CurrentProtocal pass',()=>{
     let test1='https://github.com/karma-runner/karma'
-    
-    console.log(utils._replaceUrlProtocal2CurrentProtocal(test1,true))
-    console.log(utils._replaceUrlProtocal2CurrentProtocal(test1))
+    expect(utils._replaceUrlProtocal2CurrentProtocal(test1,true)).toBe(`${window.location.protocol}//github.com/karma-runner/karma`)
+    expect(utils._replaceUrlProtocal2CurrentProtocal(test1)).toBe(`github.com/karma-runner/karma`)
   })
 })
