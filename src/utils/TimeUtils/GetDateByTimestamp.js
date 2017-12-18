13c78773e8943e29609d049cd5f5cfc0c3fb02c2
rhymedys <rhymedys@gmail.com>
2017-12-18 20:25:25 +0800

    update

--- src/utils/TimeUtils/GetDateByTimestamp.js
+++ src/utils/TimeUtils/GetDateByTimestamp.js
@@ -2,10 +2,10 @@
  * @Author: Rhymedys/Rhymedys@gmail.com 
  * @Date: 2017-12-16 15:42:20 
  * @Last Modified by: Rhymedys
- * @Last Modified time: 2017-12-18 10:05:18
+ * @Last Modified time: 2017-12-18 20:24:38
  */
 import injectDateFormatToDateAndCallback from './InjectDateFormatToDateAndCallback'
-// import {defaultDateFormatType} from '../Constant/DefaultDateFormatType'
+import {defaultDateFormatType} from '../Constant/DefaultDateFormatType'
 
  /**
  * @description 将时间戳转换成标准日期
@@ -14,5 +14,5 @@ import injectDateFormatToDateAndCallback from './InjectDateFormatToDateAndCallba
  * @returns 
  */
 export default function  _getDateByTimestamp (numTimestamp, formatType) {
-  return injectDateFormatToDateAndCallback(()=>new Date(numTimestamp).format(formatType || 'yyyy-MM-dd hh:mm:ss'))
+  return injectDateFormatToDateAndCallback(()=>new Date(numTimestamp).format(formatType || defaultDateFormatType))
 }
