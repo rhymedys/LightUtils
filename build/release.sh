fe87dd0a4ca89af1d5b8cfe0d989ae709e5cfa03
rhymedys <rhymedys@gmail.com>
2018-03-19 17:38:45 +0800

    [build] v0.0.6,

--- build/release.sh
+++ build/release.sh
@@ -1,11 +1,7 @@
-set -e
+VERSION=`cat package.json|awk -F"[:]" '/version/{print $2}' | sed 's/\"//g' | sed 's/^[ \t]*//g' `
+echo $VERSION
 
-if [[ -z $1 ]]; then
-  echo "Enter new version: "
-  read VERSION
-else
-  VERSION=$1
-fi
+set -e
 
 read -p "Releasing v$VERSION - are you sure? (y/n)" -n 1 -r
 echo
@@ -16,12 +12,13 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   VERSION=$VERSION npm run build
 
   # commit
-  # git add -A
-  # git commit -m "[build] v$VERSION"
+  
+  git add -A
+  git commit -m "[build] v$VERSION"
   npm version $VERSION --message "[release] $VERSION"
 
   # publish
   # git push eleme refs/tags/v$VERSION
   # git push eleme master
-  npm publish
+  # npm publish
 fi
